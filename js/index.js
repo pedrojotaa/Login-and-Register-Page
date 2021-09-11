959b02cf5135d35f1957b246531466c0d5d74df3
Joao Pedro <eng.joao.pedro@outlook.com>
2021-09-10 23:23:04 -0300

    organizing the page

---
+++ js/index.js
@@ -0,0 +1,7 @@
+//pagina de inicio
+
+let buttonInicio = document.querySelector('#innerButton')
+
+buttonInicio.addEventListener('click', ()=>{
+    window.location.href = 'http://localhost:5000/login.html?'
+})
